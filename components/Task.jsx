f42522d08953ec242b3673ca4ddba0722609d4a2
lipocodes <lipocodes@yahoo.com>
2023-05-25 15:01:48 +0300

    stage6

--- components/Task.jsx
+++ components/Task.jsx
@@ -23,8 +23,8 @@ const Task = ({stageNumber,index, updateDB, indexRenderedTasks, setIndexRendered
   
   const [inputConnectBoard, setInputConnectBoard] = useState("");
   const [inputName, setInputName] = useState("");
-  const [inputstatus, setInputStatus] = useState("");
-
+  const [inputStatus, setInputStatus] = useState("");
+  
   
 
   setTimeout(()=>{
@@ -36,33 +36,53 @@ const Task = ({stageNumber,index, updateDB, indexRenderedTasks, setIndexRendered
       switch(stageNumber){
         case '1': 
           setInputName(coaching.stage1.tasks[index].name);
+          setInputStatus(coaching.stage1.tasks[index].status);
+          setInputConnectBoard(coaching.stage1.tasks[index].connectBoard);
           break;
         case '2': 
           setInputName(coaching.stage2.tasks[index].name);
+          setInputStatus(coaching.stage2.tasks[index].status);
+          setInputConnectBoard(coaching.stage2.tasks[index].connectBoard);
           break;
         case '3': 
           setInputName(coaching.stage3.tasks[index].name);
+          setInputStatus(coaching.stage3.tasks[index].status);
+          setInputConnectBoard(coaching.stage3.tasks[index].connectBoard);
           break;
         case '4': 
           setInputName(coaching.stage4.tasks[index].name);
+          setInputStatus(coaching.stage4.tasks[index].status);
+          setInputConnectBoard(coaching.stage4.tasks[index].connectBoard);
           break;
         case '5': 
           setInputName(coaching.stage5.tasks[index].name);
+          setInputStatus(coaching.stage5.tasks[index].status);
+          setInputConnectBoard(coaching.stage5.tasks[index].connectBoard);
           break; 
         case '6': 
           setInputName(coaching.stage6.tasks[index].name);
+          setInputStatus(coaching.stage6.tasks[index].status);
+          setInputConnectBoard(coaching.stage6.tasks[index].connectBoard);
           break;
         case '7': 
           setInputName(coaching.stage7.tasks[index].name);
+          setInputStatus(coaching.stage7.tasks[index].status);
+          setInputConnectBoard(coaching.stage7.tasks[index].connectBoard);
           break;
         case '8': 
           setInputName(coaching.stage8.tasks[index].name);
+          setInputStatus(coaching.stage8.tasks[index].status);
+          setInputConnectBoard(coaching.stage8.tasks[index].connectBoard);
           break;
         case '9': 
           setInputName(coaching.stage9.tasks[index].name);
+          setInputStatus(coaching.stage9.tasks[index].status);
+          setInputConnectBoard(coaching.stage9.tasks[index].connectBoard);
           break; 
         case '10': 
           setInputName(coaching.stage10.tasks[index].name);
+          setInputStatus(coaching.stage10.tasks[index].status);
+          setInputConnectBoard(coaching.stage10.tasks[index].connectBoard);
           break;                
         default: break;  
       } 
@@ -110,7 +130,10 @@ const Task = ({stageNumber,index, updateDB, indexRenderedTasks, setIndexRendered
       <FontAwesomeIcon icon={faComment }  size="xl"  style={{color:'#FFD700'}}/>
     </td>
     <td className="px-6 py-4" >
-    <span onClick={setTaskStatus}>{status}</span>
+    <span onClick={setTaskStatus}>
+      {inputStatus}
+
+    </span>
     {isTaskStatusOpen && (
        <div className='bg-white flex flex-wrap static w-40'>
          <p className='bg-orange-400 w-40 text-center mt-2 p-2 rounded-md'>משימה חדשה</p>
@@ -131,7 +154,39 @@ const Task = ({stageNumber,index, updateDB, indexRenderedTasks, setIndexRendered
       <FontAwesomeIcon icon={faUserCircle }  size="xl" style={{color:'#008B8B'}} />
     </td>
     <td className="px-6 py-4">
-      {connectBoard}
+      
+    <input
+      className='border'
+      type="text"
+      value={inputConnectBoard}
+      onChange={(event)=>{
+          setInputConnectBoard (event.target.value);
+      }}
+      onKeyDown={(event)=>{
+          if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+            event.preventDefault();
+          }
+        }
+      }
+      onBlur={(event)=> {
+               const val = event.target.value; 
+               switch(stageNumber){
+                case '1': coaching.stage1.tasks[index].connectBoard = val;  break;
+                case '2': coaching.stage2.tasks[index].connectBoard  = val; break;
+                case '3': coaching.stage3.tasks[index].connectBoard  = val; break;
+                case '4': coaching.stage4.tasks[index].connectBoard  = val; break;
+                case '5':  coaching.stage5.tasks[index].connectBoard  = val; break;
+                case '6': coaching.stage6.tasks[index].connectBoard  = val; break;
+                case '7': coaching.stage7.tasks[index].connectBoard  = val; break;
+                case '8': coaching.stage8.tasks[index].connectBoard  = val; break;
+                case '9': coaching.stage9.tasks[index].connectBoard  = val; break;
+                case '10': coaching.stage10.tasks[index].connectBoard  = val; break;
+                default: break;
+               }
+               updateDB();
+             }}  
+             />
+
     </td>
     <td className="px-6 py-4 text-center">
       <FontAwesomeIcon   icon={faFileCode }  size="xl" style={{color:'#DC143C'}}/>
