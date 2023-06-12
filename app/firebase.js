e4438d22a4e727be35e1cfa71dc9b54b797baf39
lipocodes <lipocodes@yahoo.com>
2023-06-12 07:51:40 +0300

    stage17

--- app/firebase.js
+++ app/firebase.js
@@ -6,13 +6,13 @@ import {getAuth}  from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDSl8kSDj6zyXo57AESy85NxtyjXLL4fms",
-  authDomain: "befriendme-53cf0.firebaseapp.com",
-  projectId: "befriendme-53cf0",
-  storageBucket: "befriendme-53cf0.appspot.com",
-  messagingSenderId: "1060489782014",
-  appId: "1:1060489782014:web:bae989b6dfa920f425fbaf",
-  measurementId: "G-DKFN7Q623Z"
+  apiKey: "AIzaSyAyMoOaojRrn5_rdmgZvkkGhnicBJdsjFw",
+  authDomain: "taskofine-c9b26.firebaseapp.com",
+  projectId: "taskofine-c9b26",
+  storageBucket: "taskofine-c9b26.appspot.com",
+  messagingSenderId: "78285732193",
+  appId: "1:78285732193:web:7f28494f86fb1310e56d20"
+ // measurementId: "G-DKFN7Q623Z"
 };
 
 // Initialize Firebase
